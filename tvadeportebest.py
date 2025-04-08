50d9d79fcf8e8762fddc886dbef492bf7c49e285
goumack <bayegorgui.niang@accel-tech.net>
2025-04-08 16:26:52 +0000

    Update tvadeportebest.py

--- tvadeportebest.py
+++ tvadeportebest.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import requests
-import matplotlib as plt
+import matplotlib.pyplot as plt  # ✅ Import corrigé
 
 # === MinMaxScaler maison ===
 def manual_minmax_scale(arr):
